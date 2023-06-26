ec7c2c8532cf8f329a06dbb65a819ced2ccf011f
Tonmoy Ghosh <tonmoy.mec@gmail.com>
2023-06-26 16:07:02 +0600

    token verification

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    res.status(401).send({error: true, message: 'unauthorized access'});
+  }
+  const token = authorization.split(' ')[1];
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
