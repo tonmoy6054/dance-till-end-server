843ae7fb70077f499bec906dfd297324469f60ea
Tonmoy Ghosh <tonmoy.mec@gmail.com>
2023-06-28 19:05:21 +0600

    payment post created

--- index.js
+++ index.js
@@ -133,6 +133,11 @@ res.send(result);
     })
    })
 
+   app.post('/payments', async(req, res)=>{
+    const payment = req.body;
+    const result = await paymentCollection.insertOne(payment);
+   })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
